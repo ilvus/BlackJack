24f44de95a5dc0654394fe1946c035008c05e2cf
ilvus <azamat.ibragiomov3102@gmail.com>
2023-02-12 13:51:36 +0600

    Second commit

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,3 @@
-
-
 import random as rd
 
 def deal_cards():
@@ -17,9 +15,48 @@ def calculate_score(card):
     a = sum(card)
     if a == 21 and len(card) == 2:
         return 0
-    elif a > 21 and 11 in a:
+    if a > 21 and 11 in card:
         card.remove(11)
         card.append(1)
+        return sum(card)
     return a
+print(f"Computer cards: {computer_cards}")
+retake = True
+while retake:
 
-calculate_score(user_cards)
+    if calculate_score(user_cards) < 21 and calculate_score(user_cards) != 0:
+        print(user_cards)
+        new_card = input("Do you want to drow another card? print 'y' or 'n': ")
+        if new_card == "y":
+            user_cards.append(deal_cards())
+        elif new_card == "n":
+            retake = False
+    else:
+        retake = False
+
+while calculate_score(computer_cards) < 17:
+    computer_cards.append(deal_cards())
+
+def compare():
+    if calculate_score(user_cards) == 0:
+        print(f"This is your cards: {user_cards}")
+        print(f"This is computer cards: {computer_cards}")
+        return "You win!"
+    elif calculate_score(user_cards) > 21:
+        print(f"This is your cards: {user_cards}")
+        print(f"This is computer cards: {computer_cards}")
+        return "You lose!"
+    elif calculate_score(computer_cards) == 0:
+        print(f"This is your cards: {user_cards}")
+        print(f"This is computer cards: {computer_cards}")
+        return "You lose!"
+    elif calculate_score(computer_cards) > 21:
+        print(f"This is your cards: {user_cards}")
+        print(f"This is computer cards: {computer_cards}")
+        return "You win!"
+    elif calculate_score(computer_cards) == calculate_score(user_cards):
+        print(f"This is your cards: {user_cards}")
+        print(f"This is computer cards: {computer_cards}")
+        return "Draw"
+    
+print(compare())
